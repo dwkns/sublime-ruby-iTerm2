34f05e5cc83c1846942c72efaabfbeec0faaae79
dwkns <dwkns@me.com>
2013-10-06 01:36:35 +0100

    First add of the dev files

---
+++ ruby-terminal.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+osascript <<END 
+property my_delay : 0.5
+on is_running(appName)
+	tell application "System Events" to (name of processes) contains appName
+end is_running
+
+set isItRunning to is_running("Terminal")
+
+if isItRunning then
+	tell application "Terminal"
+		activate
+		if (window 1 exists) then
+			
+			-- check to see if it's busy
+			if (window 1 is busy) then
+				-- It is busy so open a new tab 
+				tell application "System Events" to keystroke "t" using command down
+			else
+				set my_delay to 0
+			end if
+			
+		else
+			-- create it
+			tell application "System Events" to keystroke "n" using command down
+		end if
+	end tell
+else
+	tell application "Terminal"
+		activate
+	end tell
+end if
+
+
+tell application "Terminal"
+activate
+	delay my_delay
+	-- do script "echo hello" in window 1
+	 do script "ruby $1" in window 1
+end tell
+END
